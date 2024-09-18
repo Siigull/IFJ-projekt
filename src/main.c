5f587d258504fffff373df7635ae5e0ef10a0e1b
Martin Vaculik <mvaculik4@gmail.com>
2024-09-18 12:01:12 +0200

    martin vaculik added email iin comment

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
  * @author Daniel Pelánek (xpeland00@stud.fit.vutbr.cz)
  * @author David Bujzaš
  * @author Jakub Havlík
- * @author Martin Vaculík
+ * @author Martin Vaculík (xvaculm00@stud.fit.vutbr.cz)
  * @brief
  * @date 2024-09-15
  *
