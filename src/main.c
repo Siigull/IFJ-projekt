a64078b4057761241091252759b83a2ee8c23a89
Jakub Havlik <xhavlij00@stud.fit.vutbr.cz>
2024-09-22 18:00:27 +0200

    Added email to main file, added header enumerating errors

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 /**
  * @file main.c
  * @author Daniel Pelánek (xpeland00@stud.fit.vutbr.cz)
- * @author David Bujzaš
- * @author Jakub Havlík
+ * @author David Bujzaš 
+ * @author Jakub Havlík (xhavlij00@stud.fit.vutbr.cz)
  * @author Martin Vaculík (xvaculm00@stud.fit.vutbr.cz)
  * @brief
  * @date 2024-09-15
