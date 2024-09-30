94822ac3a600a8b06d24acf57c4db4e8dc643b7d
xpeland00 <dan@dhcpe083.fit.vutbr.cz>
2024-09-30 14:50:08 +0200

    Took care of David Bujzaš

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 /**
  * @file main.c
  * @author Daniel Pelánek (xpeland00@stud.fit.vutbr.cz)
- * @author David Bujzaš
  * @author Jakub Havlík (xhavlij00@stud.fit.vutbr.cz)
  * @author Martin Vaculík (xvaculm00@stud.fit.vutbr.cz)
  * @brief
