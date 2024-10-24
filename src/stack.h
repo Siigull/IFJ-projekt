8a14f8dbc9f7403587e0e432025f0e1b48d9919f
Jakub Havlík <xhavlij00@stud.fit.vutbr.cz>
2024-10-24 15:37:48 +0200

    added prototypes of function into stack.h

--- src/stack.h
+++ src/stack.h
@@ -18,3 +18,32 @@ typedef struct List{
     List_elem* act;
     int length;
 }List;
+
+void List_init(List *list);
+void List_delete(List *list);
+
+//insertions
+void DLL_insertF(List *list, Token* data);
+void List_insertL(List *list, Token* data);
+void List_insert_after(List *list, Token* data);
+void List_insert_before(List *list, Token* data);
+
+
+//removing elements
+void List_removeF(List *list);
+void List_removeL(List *list);
+void List_remove_after(List *list);
+void List_remove_before(List *list);
+
+//activity functions
+void List_activeF(List *list);
+void List_activeL(List *list);
+void List_active_next(List *list);
+void List_active_prev(List *list);
+bool List_is_active(List *list);
+
+//get token pointers
+void List_first_val(List *list, Token *dataPtr);
+void List_last_val(List *list, Token *dataPtr);
+void List_get_val(List *list, Token *dataPtr);
+void List_set_val(List *list, Token* data);
