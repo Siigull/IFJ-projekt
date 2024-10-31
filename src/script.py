0b66b4fed78fb0b7dadaab2238e75ecb7a73f7ae
Siigull <pelanekd@gmail.com>
2024-10-31 23:37:24 +0100

    almost working script

---
+++ src/script.py
@@ -0,0 +1,30 @@
+import os
+import re
+
+def generate_hh_file(h_file_path):
+    with open(h_file_path, 'r') as file:
+        h_content = file.readlines()
+    
+    hh_file_path = h_file_path.replace('.h', '.hhh')
+    
+    found = 0
+
+    with open(hh_file_path, 'w') as hh_file:
+        for line in h_content:
+            if re.search("#*_H", line):
+                hh_file.write(line[:-1] + "H\n")
+                found += 1
+            else:
+                if found == 2:
+                    found += 1
+                    hh_file.write('extern "C" {\n')
+
+                if "#endif" in line:
+                    hh_file.write("\n}\n")
+                
+                if re.match("#include *\.h\"", line):
+                    line = line[:-2] + "h\n" 
+
+                hh_file.write(line)
+
+generate_hh_file("expressionparser.h")
