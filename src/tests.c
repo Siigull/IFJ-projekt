cfb1ec59206cc3426df962bd4c40bca4e6b8be25
Siigull <pelanekd@gmail.com>
2024-11-19 00:12:44 +0100

    more testing

--- src/tests.c
+++ src/tests.c
@@ -86,7 +86,7 @@ int main() {
 
 	// generate_debug_token_arr();
 
-	char* temp = load_inputos("examples/input8.8.ifj");
+	char* temp = load_inputos("src/test.ifj");
 	compile(temp);	
 	return 0;
 };
